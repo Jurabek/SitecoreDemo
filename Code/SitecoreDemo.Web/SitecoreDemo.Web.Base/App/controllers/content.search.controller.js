3318601e5acb0254d25c441c095ce69cf117d26a
isabilal <isabilal@gmail.com>
2016-07-04 19:23:00 +0200

    master: removed  from content.search.controller.js

--- Code/SitecoreDemo.Web/SitecoreDemo.Web.Base/App/controllers/content.search.controller.js
+++ Code/SitecoreDemo.Web/SitecoreDemo.Web.Base/App/controllers/content.search.controller.js
@@ -1,5 +1,5 @@
 ﻿angular.module('app').controller('ContentSearchController',
-    function ($scope, $location, contentSearchService) {
+    function ($scope, contentSearchService) {
         $scope.searchResultsUrl = contentSearchService.searchResultsUrl();
         $scope.searchClick = function () {
             window.location.href = $scope.searchResultsUrl + '?q=' + encodeURIComponent($scope.keyword);
